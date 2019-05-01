78ea9d4ce22c13fd725762637304e609a1ce4470
Mike Litzow <litzow@faralloninstitute.org>
2019-05-01 11:00:14 -0800

    compare with lme

--- environment era regression hier.r
+++ environment era regression hier.r
@@ -11,6 +11,7 @@ dat$npgo <- win.npgo[match(dat$year, names(win.npgo))]
 library(reshape2)
 library(glmmTMB)
 library(rstanarm)
+library(lme4)
 
 # reshape with year, era, and pdo and npgo as the grouping variables
 melted <- melt(dat, id.vars = c("year","pdo","era","npgo"))
@@ -37,4 +38,53 @@ mod = glmmTMB::glmmTMB(pdo ~ (-1 + (era:z_value)|variable), data=melted,
 
 mod = rstanarm::stan_lmer(pdo ~ (1+ z_value + era:z_value|variable), data=melted)
 
-coef(mod)
+# make a df to hold results for plotting
+plot <- as.data.frame(coef(mod)$variable[,1:2]) %>%
+  gather()
+
+plot$era <- ifelse(plot$key=="z_value", "Era.1", "Era.2")
+
+# make plot-friendly label names
+names <- c("Spring SST", "Winter SST", "Summer NW wind", "Summer SE wind", "Winter NW wind", "Winter SE wind",
+           "Ice 57.75N 164.5W", "Ice 58.75N 164.5W", "Ice 64.25N 163.5W")
+
+
+plot$names <- names
+plot$names <- reorder(plot$names, rep(plot$value[plot$era=="Era.1"],2))
+
+
+ggplot(plot, aes(value, fill=era)) + 
+  theme_linedraw() +
+  geom_density(alpha=0.8)
+
+ggplot(plot, aes(names, value, fill=era)) + 
+  theme_linedraw() +
+  geom_bar(position="dodge", stat="identity") +
+  theme(axis.text.x = element_text(angle = 45, hjust=1), axis.title.x = element_blank(),
+        legend.position = c(0.2, 0.8), legend.title = element_blank())
+
+
+# compare with lme results
+mod <- lmer(pdo ~ -1 + era:z_value + (-1 + era:z_value | variable),  data=melted)
+ranef(mod)
+
+# combine and plot
+plot$method <- "stan_lmer"
+
+plot2 <- as.data.frame(coef(mod)$variable) %>%
+  gather()
+
+plot2$era <- ifelse(plot2$key=="era1:z_value", "Era.1", "Era.2")
+plot2$names <- names
+plot2$method <- "lmer"
+
+plot <- rbind(plot, plot2)
+
+png("method comparison", 4, 4, units="in", res=300)
+ggplot(plot, aes(names, value, fill=era)) + 
+  theme_linedraw() +
+  geom_bar(position="dodge", stat="identity") +
+  theme(axis.text.x = element_text(angle = 45, hjust=1), axis.title.x = element_blank(),
+        legend.position = c(0.1, 0.9), legend.title = element_blank()) +
+  facet_wrap(~method, ncol=1)
+dev.off()
