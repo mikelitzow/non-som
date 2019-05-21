96e1bcceb191493ebd9a2dfb10eb6cbb88ea783f
Mike Litzow <mlitzow@alaska.edu>
2019-05-21 13:43:17 -0800

    Added PDO-NPGO correlation plot.

--- node time series plot.R
+++ node time series plot.R
@@ -1,5 +1,9 @@
 library(tidyverse)
+library(mgcv)
+
 cb <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# first, pdo/npgo node plots
 dat <- read.csv("SKT_Classes.csv", na.strings = "FALSE")
 
 head(dat)
@@ -69,11 +73,7 @@ map('world2Hires',fill=F, xlim=c(130,250), ylim=c(20,66),add=T, lwd=1)
 # set names
 dimnames(SST) <- list(as.character(d.t), paste("N", lat.t, "E", lon.t, sep=""))
 
-###########################
-# calculate the various time series
-###########################
-
-# first, mean  temp
+# calculate time series
 
 f <- function(x) tapply(x, m[yr %in% 1951:1980], mean)  # function to compute monthly means for a single time series
 
@@ -93,24 +93,56 @@ SST.anomTS <- apply(SST.anom,1,ff)   # Compute monthly anomalies!
 
 dec.yr.t <- as.numeric(as.character(yr)) + (as.numeric(m)-0.5)/12
 
-plot(dec.yr.t, SST.anomTS, type="l")
-abline(h=0)
-
 # sst time series plot
 use.dat <- data.frame(yr=dec.yr.t, sst=SST.anomTS)
 use.dat$era <- as.factor(ifelse(use.dat$yr<1989,1,2))
 
-pred.sst1 <- predict(lm(sst ~ yr:era + era, data=use.dat))
-
-pred.sst2 <- predict(gam(sst ~ s(yr), data=use.dat))
-
-pred.sst3 <- rollmean(SST.anomTS, 133, fill=NA)
+pred.sst <- predict(gam(sst ~ s(yr), data=use.dat))
 
 png("ne pacific sst plot.png", 5,4, units="in", res=300)
 par(mar=c(4,4,1.5,1),las=1)
-plot(dec.yr.t, SST.anomTS, type="l", xlab="", ylab="ºC wrt 1951-1980", xlim=xlim, col=cb[6])
-lines(dec.yr.t, pred.sst2, col="red")
+plot(dec.yr.t, SST.anomTS, type="l", xlab="", ylab="ºC wrt 1951-1980", col=cb[6])
+lines(dec.yr.t, pred.sst, col="red")
 abline(h=0)
 abline(v=1989, lty=2)
 dev.off()
 
+####
+# now pdo-npgo correlations
+
+# load TS
+# load pdo
+names <- read.table("~pdo", skip=30, nrows=1, as.is = T)
+pdo <- read.table("~pdo", skip=31, nrows=119, fill=T, col.names = names)
+pdo$YEAR <- 1900:(1899+nrow(pdo)) # drop asterisks!
+pdo <- pdo %>%
+  gather(month, value, -YEAR) %>%
+  arrange(YEAR)
+
+# load npgo
+# download.file("http://www.oces.us/npgo/npgo.php", "~npgo")
+npgo <- read.table("~npgo", skip=10, nrows=828, fill=T, col.names = c("Year", "month", "value"))
+pdoTS <-pdo[601:1425,]
+npgoTS <- npgo[1:825,]
+rownames(pdoTS) <- rownames(npgoTS) <- 1:nrow(pdoTS)
+npgo$dec.yr <- npgo$Year + (npgo$month-0.5)/12
+
+# create object to catch results and loop through
+pdo.npgo.plot <- data.frame(dec.yr=NA, cor=NA)
+
+for(i in 127:(nrow(pdoTS)-126)){
+  temp <- data.frame(dec.yr=NA, cor=NA)
+  
+  pdo.npgo.plot$cor[(i-126)] <- cor(pdoTS$value[(i-126):(i+126)], npgoTS$value[(i-126):(i+126)])
+  pdo.npgo.plot$dec.yr[(i-126)] <- npgo$dec.yr[i]
+
+  pdo.npgo.plot<- rbind(pdo.npgo.plot, temp)  
+}
+
+png("pdo npgo corr plot.png", 5,4, units="in", res=300)
+par(mar=c(4,4,1.5,1),las=1)
+plot(pdo.npgo.plot$dec.yr, pdo.npgo.plot$cor, type="n", xlab="", ylab="Correlation")
+abline(h=0)
+abline(v=1989, lty=2)
+lines(pdo.npgo.plot$dec.yr, pdo.npgo.plot$cor,  col=cb[6])
+dev.off()
